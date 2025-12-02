65f7e5aafbe5b059845cfd71f5277de75e83ae12
Briananto, Aditya <aditya.briananto@lazada.co.id>
2025-12-02 14:41:14 +0700

    update 3pl prefix

--- config/handover.php
+++ config/handover.php
@@ -1,18 +1,20 @@
 <?php
 
 $prefixMap = [
-    'JNE' => ['CM', 'JT', 'TG', '151', '881', 'JY'],
+    'JNE' => ['CM', 'JT', 'TG', '151', '881', 'JY','JNEB'],
     'ShopeeXpress' => ['SPX'],
     'SICEPAT' => ['004', '002', '005', 'TKP5'],
-    'SAPX' => ['TKP4'],
-    'JNT Cargo' => ['570'],
-    'JNT Express' => ['JX'],
+    'SAPX' => ['TKP4','TKP5'],
+    'JNT Cargo' => ['570','320'],
+    'JNT Express' => ['JX','JO'],
     'Anteraja' => ['TSA'],
     'ID Express' => ['TKP8'],
     'LEX ID' => ['LID'],
-    'LEX' => ['LXAD', 'NLIDAP', 'JZ'],
+    'LEX' => ['LXAD', 'NLIDAP', 'JZ','JNAP'],
     'GTL' => ['GTL'],
     'REX' => ['2222'],
+    'SF EXPRESS' => ['ZLM'],
+    'Ninjavan Standard' => ['SHP'],
 ];
 
 return [
